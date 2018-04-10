0480c03975b4635f76ff1aa28795c13e0c7c7aab
Toni Väisänen <vaisanen.toni@gmail.com>
2018-04-10 09:19:18 +0200

    google ads

--- src/components/StartScreen.js
+++ src/components/StartScreen.js
@@ -9,15 +9,15 @@ class StartScreen extends Component {
         return (
             <div style={{margin: "10vh"}}>
                 <h2>Ear Trainer for Audio Engineers</h2>
-                <p>This is a ear trainer application aimed to help aspiring audio
-                    engineers to train their ears to recognize different
+                <p>This is a ear trainer application for audio
+                    engineers to train their ears in recognizing different
                     filter types and filter cut off frequencies.
-                    At this moment we are still in the beta phase. </p>
+                    </p>
                 <h3>How to get started?</h3>
                 <p>Open exercise menu by clicking the menu icon in the top bar
                     and select an exercise.</p>
                 <h3>Feedback or Questions?</h3>
-                <p>You can contact us by email: EQEarTrainer (at) gmail.com</p>
+                <p>EQEarTrainer (at) gmail.com</p>
             </div>
         )
     }
--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-
+import firebase from 'firebase';
 
 
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
@@ -12,6 +12,19 @@ import darkBaseTheme from 'material-ui/styles/baseThemes/darkBaseTheme';
 // This replaces the textColor value on the palette
 // and then update the keys for each component that depends on it.
 // More on Colors: http://www.material-ui.com/#/customization/colors
+
+
+var config = {
+  apiKey: "AIzaSyCsY5V6Uy7V9SHPYF7v9O5xSTiYiMwRbHg",
+  authDomain: "eq-ear.firebaseapp.com",
+  databaseURL: "https://eq-ear.firebaseio.com",
+  projectId: "eq-ear",
+  storageBucket: "",
+  messagingSenderId: "677468603973"
+};
+firebase.initializeApp(config);
+
+
 
 window.debug = true;
 
